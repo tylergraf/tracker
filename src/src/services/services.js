8f367e7f755f58b50cc89e65ef773c2093d2b1f0
Tyler Graf <tylergraf@gmail.com>
2017-09-24 00:09:00 -0600

    Sort track list

--- src/src/services/services.js
+++ src/src/services/services.js
@@ -35,10 +35,10 @@
   };
 
   const fetchData = function(url, config){
-    // const currentUser = firebase.auth().currentUser;
-    // if(!currentUser){
-    //   return Promise.reject(401);
-    // }
+    const currentUser = firebase.auth().currentUser;
+    if(!currentUser){
+      return Promise.reject(401);
+    }
 
     config = config || {};
     const defaults = {
@@ -51,11 +51,11 @@
 
     config = Object.assign(defaults, config);
 
-    // return currentUser.getIdToken()
-    //   .then(idToken=>{
-        config.headers.authorization = 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc0NWM3MTI4Y2JhMTBlMjUxYjlmZTcxMmFlZDUyNjEzMzg4YTY2OTkifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vdHJhY2tlci0zYjQwYiIsIm5hbWUiOiJUeWxlciBHcmFmIiwicGljdHVyZSI6Imh0dHBzOi8vbGg2Lmdvb2dsZXVzZXJjb250ZW50LmNvbS8tT09WbUJfNkdjdVkvQUFBQUFBQUFBQUkvQUFBQUFBQUFBWEEvTlQteW9uODVVUHMvcGhvdG8uanBnIiwiYXVkIjoidHJhY2tlci0zYjQwYiIsImF1dGhfdGltZSI6MTUwNTc5NDMxOSwidXNlcl9pZCI6IjNPb0g1VEtubHVVNjJ6NlVuQ25kcE01V1d2WTIiLCJzdWIiOiIzT29INVRLbmx1VTYyejZVbkNuZHBNNVdXdlkyIiwiaWF0IjoxNTA1ODA0NTYzLCJleHAiOjE1MDU4MDgxNjMsImVtYWlsIjoidHlsZXJncmFmQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7Imdvb2dsZS5jb20iOlsiMTEzNjM4NDkzMjIwNDY3MDE4NDIxIl0sImVtYWlsIjpbInR5bGVyZ3JhZkBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJnb29nbGUuY29tIn19.BNBa-Vc7DHXeLZrh5qqMTMFRjPo6DiAXP_sXUUfU_OE9L6JwUUtpGv8kG8RWkz3Q_0WXbqhe51YSA-hyhyJmkBhWzDoq72WGBBw6YF50fqNCLgAKz7p4Oqzu1D77h_J_nlFjKu3hdKxsAn25LZdn5jksLir0vIF9Np37j0FVnKytI4TK05Yj8soe-nqw5TtQ0_YT6m-rVGcOopqKLGqhf88UtIthf59WKWP8y8JAZ-q1Gdu4dSg5mSN6ZLbuFDpVZUZM3cuOwiFAzl8WQ-j-aVuRG3DM3sQnXy2uMrHneZV4ArxJ8ktfd9JAGur5Bjamv54MaKCJ8u6gejRfKoJ0eA';
+    return currentUser.getIdToken()
+      .then(idToken=>{
+        config.headers.authorization = `Bearer ${idToken}`;
         return fetch(url,config)
-      // })
+      })
       .then(res=>{
         if(res.status === 204){
           return;
